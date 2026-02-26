0f08a059ea3fc65ec3e912bbc1dd49452d84fe49
Mr. R <nseinvestama@gmail.com>
2026-02-26 11:31:02 +0800

    Enlarge profile photo to 180px and refine verified badge position

--- deploy_final_v10.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Clean profile image: remove border, keep verified badge"
-git push
---
+++ deploy_final_v11.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Enlarge profile photo to 180px and refine verified badge position"
+git push
