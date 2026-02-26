b9fc59434913ab9b41147807cb1c48a5428389e0
Mr. R <nseinvestama@gmail.com>
2026-02-26 11:33:16 +0800

    Update social link text to simple WHATSAPP and INSTAGRAM

--- deploy_final_v11.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Enlarge profile photo to 180px and refine verified badge position"
-git push
---
+++ deploy_final_v12.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Update social link text to simple WHATSAPP and INSTAGRAM"
+git push
