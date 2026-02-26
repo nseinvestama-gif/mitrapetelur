d7e373a55d7fe289bbfabc7ac28da74f1b900eef
Mr. R <nseinvestama@gmail.com>
2026-02-26 11:04:31 +0800

    Wrap distribution and package images in cards with updated text

--- deploy_final_v5.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Add detailed partnership info section"
-git push
---
+++ deploy_final_v6.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Wrap distribution and package images in cards with updated text"
+git push
