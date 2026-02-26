ca71bb0ba0801d8b9a3bbad1ad7118c5425325f2
Mr. R <nseinvestama@gmail.com>
2026-02-26 10:57:37 +0800

    Update info box content to match provided text and style

--- deploy_final_v3.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Add Rantai Distribusi & Pilihan Paket images, and refactor package cards to match screenshot"
-git push
---
+++ deploy_final_v4.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Update info box content to match provided text and style"
+git push
