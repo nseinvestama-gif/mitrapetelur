494f86ffcafa1baae0832245f7d6ef3f40dc8aea
Mr. R <nseinvestama@gmail.com>
2026-02-26 11:14:25 +0800

    Refactor layout: Move Cara Bergabung images up, add new Footer card with social icons

--- deploy_final_v6.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Wrap distribution and package images in cards with updated text"
-git push
---
+++ deploy_final_v7.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Refactor layout: Move Cara Bergabung images up, add new Footer card with social icons"
+git push
