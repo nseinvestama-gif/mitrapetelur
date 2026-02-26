e8a1ed17b3900b76b06d62af65c5ffc64f256ed6
Mr. R <nseinvestama@gmail.com>
2026-02-26 11:25:34 +0800

    Update profile: larger image with verified badge, new contact info for WA and IG

--- deploy_final_v8.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Refactor layout: Restore distribution & package images position, move 'Cara Bergabung' detail section above package plan"
-git push
---
+++ deploy_final_v9.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Update profile: larger image with verified badge, new contact info for WA and IG"
+git push
