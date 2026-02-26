b669ee40ef96ea74f4e97e061375a255c6e8834b
Mr. R <nseinvestama@gmail.com>
2026-02-26 11:20:25 +0800

    Refactor layout: Restore distribution & package images position, move 'Cara Bergabung' detail section above package plan

--- deploy_final_v7.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Refactor layout: Move Cara Bergabung images up, add new Footer card with social icons"
-git push
---
+++ deploy_final_v8.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Refactor layout: Restore distribution & package images position, move 'Cara Bergabung' detail section above package plan"
+git push
